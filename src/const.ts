85ae41e62bab2a30afb2a3e5efe719fc1917dda5
Miguel Muñiz <73082309+MiguelMunizJR@users.noreply.github.com>
2024-01-31 03:37:02 -0600

    add scores page and remove pre-fetching of logout

--- src/const.ts
+++ src/const.ts
@@ -1,25 +1,41 @@
-interface ROUTES_PATH {
-	HOME: string;
-	LOGIN: string;
-	SIGNUP: string;
-	USER: string;
-	DASHBOARD: string;
-	NOT_FOUND: string;
+interface IApiRoutes {
+  LOGIN: string;
+  LOGOUT: string;
+  SIGNUP: string;
 }
 
-export const URL_API = "https://musicians-corner-app.onrender.com/api/v1";
+interface IRoutesPath {
+  HOME: string;
+  LOGIN: string;
+  SIGNUP: string;
+  DASHBOARD: string;
+  LIBRARY: string;
+  USER: string;
+  SCORES: string;
+  METRONOME: string;
+  TUNER: string;
+  SETTINGS: string;
+  NOT_FOUND: string;
+}
 
-export const ROUTES_PATH = Object.freeze({
+export const URL_API: string = "https://musicians-corner-app.onrender.com/api/v1";
+
+export const API_ROUTES: IApiRoutes = Object.freeze({
+	LOGIN: "/api/auth/login",
+	LOGOUT: "/api/auth/logout",
+	SIGNUP: "/api/auth/signup",
+});
+
+export const ROUTES_PATH: IRoutesPath = Object.freeze({
 	HOME: "/",
 	LOGIN: "/auth/login",
 	SIGNUP: "/auth/signup",
-	USER: "/users/me",
 	DASHBOARD: "/dashboard",
 	LIBRARY: "/library",
-	NOT_FOUND: "*"
-});
-
-export const API_ROUTES = Object.freeze({
-	LOGIN: "/api/auth/login",
-	SIGNUP: "/api/auth/signup",
+	USER: "/users/me",
+	SCORES: "/scores",
+	METRONOME: "/metronome",
+	TUNER: "/tuner",
+	SETTINGS: "/settings",
+	NOT_FOUND: "*",
 });
--- src/pages/api/auth/logout.ts
+++ src/pages/api/auth/logout.ts
@@ -1,9 +1,9 @@
+export const prerender = false;
+
 import { ROUTES_PATH } from "@/const";
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({ cookies, redirect }) => {
-	console.log("logout");
-	
 	cookies.delete("sb-access-token", { path: "/" });
 	cookies.delete("sb-refresh-token", { path: "/" });
 	return redirect(ROUTES_PATH.HOME);
